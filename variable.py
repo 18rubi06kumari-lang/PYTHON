a03ea8717d7c3545048bf89769d565a6a033c287
18rubi06kumari-lang <18rubi06kumari@gmail.com>
2026-07-28 19:36:15 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,13 @@
+#creating diff. variable
+x=5
+y="hello"
+
+print(x)
+print(y)
+
+codingal="codingal"
+print(codingal)
+
+
+name = input("enter your name")
+print("you have entered your name", name)
